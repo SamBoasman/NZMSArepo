56ac7e9a18472496239bdfae7a0fca5482a10c23
SamBoasman <samboasman@gmail.com>
2022-08-09 23:34:03 +1200

    feat: header, input and button now added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from "react";
+import { useState } from "react";
+
+import { Root, Root2 } from "./tsTypes/CountryTypes";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+    const [countryName, setCountryName] = useState("");
+    const [countryInfo, setCountryInfo] = useState<undefined | Root2>(
+        undefined
+    );
+
+    const COUNTRIES_URL = "https://restcountries.com/v3.1/name/";
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <h1>Country Search</h1>
+
+            <div>
+                <label>Country Name</label>
+                <br />
+                <input
+                    type="text"
+                    id="country-name"
+                    name="country-name"
+                    onChange={(e) => setCountryName(e.target.value)}
+                />
+                <br />
+                <button onClick={search}>Search</button>
+            </div>
+
+            <p>You have entered {countryName}</p>
         </div>
     );
+
+    function search() {
+        alert("Search button has been clicked!");
+    }
 }
 
 export default App;
